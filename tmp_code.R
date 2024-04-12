68ed89b243a5e636c3cdb46f25b37fb0eece962b
Francisco <fhenaod@zoology.ubc.ca>
2024-04-11 21:51:42 -0500

    harmonize code

--- tmp_code.R
+++ tmp_code.R
@@ -4,11 +4,8 @@ library(tidyverse)
 
 #Laura. Of course I can expand this line
 
-<<<<<<< HEAD
 2 + 4 = 100
-=======
-#tati 
 
->>>>>>> c35f1c6f55656285bcbe052d0f5f06436f408e66
+#tati 
 
 #Louise was here
